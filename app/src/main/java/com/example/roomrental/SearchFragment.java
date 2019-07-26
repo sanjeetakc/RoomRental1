5d3cec796e66c8132f0b19bf20f85ea637ab5d06
suranjan.paudel <suranjan.paudel@joinsage.com>
2019-07-26 21:59:22 +0545

    Search working

--- app/src/main/java/com/example/roomrental/SearchFragment.java
+++ app/src/main/java/com/example/roomrental/SearchFragment.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,26 +13,40 @@ import android.widget.SearchView;
 
 public class SearchFragment extends Fragment {
     private SearchView msearchView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search,container,false);
+        return inflater.inflate(R.layout.fragment_search, container, false);
 
-    //msearchView= findViewId(R.id.search_view);
+        //msearchView= findViewId(R.id.search_view);
     }
     /*@Override
     public void onDestroy() {
         super.onDestroy();
         ((AppCompatActivity)getActivity()).getSupportActionBar().show();}*/
 
+
+    public void onCreate() {
+        try {
+            ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
+        } catch (Exception e) {
+            Log.i("Erooor", e.getMessage());
+        }
+    }
+
     @Override
     public void onResume() {
         super.onResume();
-        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
     }
+
     @Override
     public void onStop() {
         super.onStop();
-        ((AppCompatActivity)getActivity()).getSupportActionBar().show();
+        try {
+            ((AppCompatActivity) getActivity()).getSupportActionBar().show();
+        } catch (Exception e) {
+            Log.i("Erooor", "Cannot show");
+        }
     }
 }
